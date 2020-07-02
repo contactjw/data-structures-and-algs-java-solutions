8c0de5308b834b9dcf4f7bed29f706622d17ed53
John West <contactjohnwest@gmail.com>
2020-07-02 12:57:27 -0700

    attempting to time bubble sort execution

--- Chapter3/src/BubbleSort/BubbleSortApp.java
+++ Chapter3/src/BubbleSort/BubbleSortApp.java
@@ -2,24 +2,22 @@ package BubbleSort;
 
 public class BubbleSortApp {
     public static void main(String[] args) {
-        int maxSize = 100;
+        long startTime = System.nanoTime();
+        int maxSize = 10000;
         ArrayBub arr = new ArrayBub(maxSize);
 
-        arr.insert(77);
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(00);
-        arr.insert(66);
-        arr.insert(33);
+        for (int i = 0; i < maxSize; i++) {
+            long n = (long)(Math.random()*(maxSize - 1));
+            arr.insert(n);
+        }
 
         arr.display();
 
         arr.bubbleSort();
 
         arr.display();
+
+        long endTime = System.nanoTime() - startTime;
+        System.out.println("Bubble sort took " + endTime + " to execute");
     }
 }
