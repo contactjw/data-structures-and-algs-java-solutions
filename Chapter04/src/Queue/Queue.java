e27e963a612fdc8787c537d0cb4d6b3c218322da
John West <contactjohnwest@gmail.com>
2020-07-05 14:36:09 -0700

    finished regular queue example

--- Chapter04/src/Queue/Queue.java
+++ Chapter04/src/Queue/Queue.java
@@ -7,8 +7,8 @@ public class Queue {
     private int rear;
     private int numItems;
 
-    public Queue(int s) {
-        maxSize = s;
+    public Queue(int size) {
+        maxSize = size;
         queArray = new long[maxSize];
         front = 0;
         rear = -1;
@@ -17,11 +17,33 @@ public class Queue {
 
     public void insert(long num) {
         if(rear == maxSize - 1) {
-            rear = -1;
+            rear = -1;                  //Wrap around to the front of the array, -1 is non existent but we will change
         }
-        queArray[++rear] = num;
+        queArray[++rear] = num;         //If we wrapped around, -1 will be incremented to 0, the first index.
         numItems++;
     }
 
+    public long remove() {
+        long temp = queArray[front++];
+        if(front == maxSize)
+            front = 0;
+        numItems--;
+        return temp;
+    }
 
+    public long peekFront() {
+        return queArray[front];
+    }
+
+    public boolean isEmpty() {
+        return (numItems == 0);
+    }
+
+    public boolean isFull() {
+        return (numItems == maxSize);
+    }
+
+    public int size() {
+        return numItems;
+    }
 }
