1e09869627fc0f9bedc05da9ddac2eb59a245f1b
John West <contactjohnwest@gmail.com>
2020-07-10 16:35:08 -0700

    finished my own implementation of a deque

--- Chapter04/src/Deque/Deque.java
+++ Chapter04/src/Deque/Deque.java
@@ -10,32 +10,100 @@ public class Deque {
     public Deque(int size) {
         maxSize = size;
         deque = new int[maxSize];
-        front = 0;
+        front = 1;
         rear = -1;
         numItems = 0;
-    }
 
-    public void insert(int val) {
-        if (numItems == maxSize) {
-            System.out.println("the deque is full, " + val + " was not inserted");
-            return;
+        for (int i = 0; i < maxSize; i++) {
+            deque[i] = -99;
         }
-        deque[++rear] = val;
-        numItems++;
     }
 
-    public int remove() {
-        System.out.println("removing " + peekFront() + " from deque");
+//    public void insert(int val) {
+//        if (numItems == maxSize) {
+//            System.out.println("the deque is full, " + val + " was not inserted");
+//            return;
+//        }
+//        deque[++rear] = val;
+//        numItems++;
+//    }
+
+    public void insertLeft(int val) {
+        if (!isFull()) {
+            if (front == 0) {
+                front = maxSize;
+            }
+            if (deque[--front] != -99) {
+                deque[front] = val;
+            }
+            else {
+                deque[front] = val;
+                numItems++;
+            }
+        }
+        else {
+            System.out.println("the deque is full, " + val + " was not inserted");
+        }
+    }
+
+    public void insertRight(int val) {
+        if (!isFull()) {
+            if (rear == maxSize - 1) {
+                rear = -1;
+            }
+            if (deque[++rear] != -99) {
+                deque[rear] = val;
+            }
+            else {
+                deque[rear] = val;
+                numItems++;
+            }
+        }
+        else {
+            System.out.println("the deque is full, " + val + " was not inserted");
+        }
+    }
+
+//    public int remove() {
+//        System.out.println("removing " + peekFront() + " from deque");
+//        numItems--;
+//        return (deque[front++]);
+//    }
+
+    public int removeLeft() {
+        int retVal = deque[front++];
+        if (front == maxSize) {
+            front = 0;
+        }
+        System.out.println("removing " + retVal + " from front of deque");
         numItems--;
-        return (deque[front++]);
+        return (retVal);
+    }
+
+    public int removeRight() {
+        int retVal = deque[rear--];
+        if (rear == -1) {
+            rear = maxSize - 1;
+        }
+        System.out.println("removing " + retVal + " from rear of deque");
+        numItems--;
+        return (retVal);
     }
 
     public boolean isEmpty() {
         return (numItems == 0);
     }
 
+    public boolean isFull() {
+        return (numItems == maxSize);
+    }
+
     public int peekFront() {
         return deque[front];
+    }
+
+    public int peekRear() {
+        return deque[rear];
     }
 
     public int size() {
--- Chapter04/src/Deque/DequeApp.java
+++ Chapter04/src/Deque/DequeApp.java
@@ -5,28 +5,67 @@ public class DequeApp {
         int maxSize = 5;
         Deque deque = new Deque(maxSize);
 
+        System.out.print("testing if deque is empty: ");
         if(deque.isEmpty()) {
-            System.out.println("deque is empty");
+            System.out.println("deque is empty\n");
+        }
+        else {
+            System.out.println("deque is not empty\n");
         }
 
-        deque.insert(1);
-        deque.insert(2);
-        deque.insert(3);
-        deque.insert(4);
-        deque.insert(5);
-        deque.insert(6);
+        deque.insertLeft(1);
+        deque.insertLeft(2);
+        deque.insertLeft(3);
+        deque.insertRight(4);
+        deque.insertRight(5);
+        deque.insertLeft(6);
 
-        deque.display();
 
         System.out.println("front of deque: " + deque.peekFront());
 
         System.out.println("the size of the deque is " + deque.size());
 
-        deque.remove();
+        deque.display();
 
-        System.out.println("the size of the deque is now " + deque.size());
+        System.out.println("");
+
 
         System.out.println("front of deque: " + deque.peekFront());
+
+        System.out.println("the size of the deque is " + deque.size());
+
+        deque.removeLeft();
+
+        deque.display();
+
+        System.out.println("the size of the deque is now " + deque.size() + "\n");
+
+
+
+        System.out.println("front of deque: " + deque.peekFront());
+
+        System.out.println("the size of the deque is " + deque.size());
+
+        deque.removeLeft();
+
+        deque.display();
+
+        System.out.println("the size of the deque is now " + deque.size() + "\n");
+
+
+
+
+        System.out.println("rear of deque: " + deque.peekRear());
+
+        System.out.println("the size of the deque is " + deque.size());
+
+        deque.removeRight();
+
+        deque.display();
+
+        System.out.println("the size of the deque is now " + deque.size() + "\n");
+
+
 
     }
 }
